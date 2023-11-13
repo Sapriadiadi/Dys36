6087a2c7f73b3b950b41c9ea7be6e6e120f395f1
Sapriadi <147253388+Sapriadiadi@users.noreply.github.com>
2023-11-14 01:06:27 +0800

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,14 @@
+package days36;
+import java.util.Scanner;
+public class Main {
+    
+    public static void main(String[] args) {
+    Scanner ac = new Scanner(System.in);
+    int a = ac.nextInt();
+        for(int i = 0; i< a; i++);{
+            System.out.print("* ");
+        }
+        System.out.println();
+    }
+    
+}
